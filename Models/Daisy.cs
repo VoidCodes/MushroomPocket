4f9c728a36df83a0ebef1e5d6080115f39fe9978
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-05-10 20:26:48 +0800

    refactor: Update Daisy class constructor to include base class initialization

--- Models/Daisy.cs
+++ Models/Daisy.cs
@@ -8,7 +8,7 @@ namespace MushroomPocket.Models
 {
     public class Daisy : Character
     {
-        public Daisy(int _hp, int _exp)
+        public Daisy(int _hp, int _exp): base(_hp, _exp)
         {
             CharacterName = "Daisy";
             Hp = _hp;
