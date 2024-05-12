32f5b82c99e717abd91dc5d7aa738fb1bc7c8f90
VoidCodes <harodtai123@outlook.com>
2024-05-12 22:35:39 +0800

    Added dbcontext for database

---
+++ Context/Dbcontext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MushroomPocket.Models;
+
+namespace MushroomPocket.Context
+{
+    public class Dbcontext : DbContext
+    {
+        //public DbSet<MushroomMaster> Mushroom { get; set; }
+        public DbSet<Character> Character { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=MushroomPocket;Trusted_Connection=True;");
+            optionsBuilder.UseSqlite(@"Data Source=MushroomPocket.db;");
+        }
+    }
+}
