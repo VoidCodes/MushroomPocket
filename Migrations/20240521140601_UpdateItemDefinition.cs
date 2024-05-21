39320fd32182db9256d96f675c4f6fb2a767ab23
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-05-21 22:33:16 +0800

    refactor: Update Inventory and Items models

---
+++ Migrations/20240521140601_UpdateItemDefinition.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MushroomPocket.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateItemDefinition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Effect",
+                table: "Items",
+                newName: "EffectValue");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Items",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EffectType",
+                table: "Items",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "EffectType",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "EffectValue",
+                table: "Items",
+                newName: "Effect");
+        }
+    }
+}
