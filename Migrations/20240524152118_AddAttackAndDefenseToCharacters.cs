ba75322dcd4bc3bddc3e7abf29402ba499765e12
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-05-24 23:32:38 +0800

    refactor: Add Attack and Defense properties to Character model

---
+++ Migrations/20240524152118_AddAttackAndDefenseToCharacters.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MushroomPocket.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttackAndDefenseToCharacters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Attack",
+                table: "Character",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Defence",
+                table: "Character",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attack",
+                table: "Character");
+
+            migrationBuilder.DropColumn(
+                name: "Defence",
+                table: "Character");
+        }
+    }
+}
